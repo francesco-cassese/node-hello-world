518a12803f1ddc92b80f7c424a25f8b3f2956344
Francesco Cassese <francescocassese9@gmail.com>
2026-05-20 15:40:07 +0200

    feat: completato super bonus

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
+import { cowsay } from 'cowsayjs'
+
+const funnyCowQuote = [
+    "Cosa ne pensi del latte parzialmente scremato? Io lo trovo un insulto al mio lavoro!",
+    "Scusa, mi sono persa... potresti indicarmi la strada per il fienile?",
+    "Oggi mi sento proprio in vena di fare un po' di... muuu-sica!",
+    "Muuu-viti! C'è dell'erba freschissima che ci aspetta dall'altra parte del recinto!",
+    "Guardami: sono il perfetto esempio di stile a macchie, altro che alta moda.",
+    "Il mio film preferito? Ovviamente 'Muuu-lan'!",
+    "Non guardarmi così, non ho nessuna intenzione di fare i compiti di muuu-sica oggi.",
+    "La vita nei campi è un continuo chewing-gum di erba, dall'alba al tramonto."
+];
+
+const casualQuote = Math.floor(Math.random() * funnyCowQuote.length);
+const selectedQuote = funnyCowQuote[casualQuote];
+
 const variabileTerminale = process.argv
 
-console.log(variabileTerminale);
+if (variabileTerminale[2] === '--la-perla-della-mucca') {
+    console.log(cowsay(selectedQuote));
+}
+
+
+
